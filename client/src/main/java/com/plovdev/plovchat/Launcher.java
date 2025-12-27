8b401fbffc2370e6d1f922d8627977d8ab8ffa48
Your Name <you@example.com>
2025-12-27 15:21:40 +0300

    File adding

--- client/src/main/java/com/plovdev/plovchat/Launcher.java
+++ client/src/main/java/com/plovdev/plovchat/Launcher.java
@@ -1,9 +1,26 @@
 package com.plovdev.plovchat;
 
 import javafx.application.Application;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Launcher {
+    private static final Logger log = LoggerFactory.getLogger(Launcher.class);
+
     public static void main(String[] args) {
+        try {
+            Path path = Path.of("downloads");
+            if (!Files.exists(path)) {
+                Files.createDirectory(path);
+            }
+        } catch (IOException e) {
+            log.error("Initializing err: ", e);
+        }
+
         Application.launch(PlovChatApp.class, args);
     }
 }
